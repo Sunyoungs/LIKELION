748bbdc417df94350ce4953c3874e94812a1183f
SFicheu <lucy4633@naver.com>
2026-06-08 15:09:08 +0900

    VOD Update

---
+++ VOD-Study/13. 쇼핑몰 앱 만들기/chapter13/src/components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import { Outlet, Link } from "react-router";
+import styles from "./Layout.module.css";
+
+const Layout = () => {
+  return (
+    <div className={styles.container}>
+      <Link to="/"><h1>멋사몰</h1></Link>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Layout;
---
+++ VOD-Study/13. 쇼핑몰 앱 만들기/chapter13/src/hooks/useFetchProduct.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+function useFetchProduct(productId) {
+  const [product, setProduct] = useState(null);
+  const [isProductLoading, setIsProductLoading] = useState(true);
+  const [isProductError, setIsProductError] = useState(false);
+
+  useEffect(() => {
+    async function fetchProduct() {
+      try {
+        setIsProductLoading(true);
+        const response = await fetch(`http://localhost:3000/products/${productId}`);
+        if (!response.ok) { throw new Error(""); }
+
+        const json = await response.json();
+        setProduct(json);
+        setIsProductLoading(false);
+      } catch {
+        setIsProductError(true);
+        setIsProductLoading(false);
+      }
+      
+    }
+    fetchProduct();
+  }, [productId]);
+
+  return {
+    product, isProductLoading, isProductError,
+  };
+}
+
+export default useFetchProduct;
